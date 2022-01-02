d18df205863622c3607bb9eaebfa548d78585edb
Luca Manolache <luca1230@gmail.com>
2022-01-02 11:07:53 -0800

    Now using a toml config file

--- process.py
+++ process.py
@@ -2,11 +2,13 @@ import cv2 as cv
 import numpy as np
 
 
-def process(frame, dilate=0, erode=0, low_H=0, low_S=0, low_V=0, high_H=0, high_S=0, high_V=0):
+def process(frame, config):
     frame_HSV = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-    frame_processed = cv.erode(frame_HSV, np.ones((5, 5)), iterations=erode)
-    frame_processed = cv.dilate(frame_processed, np.ones((5, 5)), iterations=dilate)
-    frame_threshold = cv.inRange(frame_processed, (low_H, low_S, low_V), (high_H, high_S, high_V))
+    frame_processed = cv.erode(frame_HSV, np.ones((5, 5)), iterations=config['erode'])
+    frame_processed = cv.dilate(frame_processed, np.ones((5, 5)), iterations=config['dilate'])
+    frame_threshold = cv.inRange(frame_processed,
+                                 np.array(config['lower_HSV']),
+                                 np.array(config['upper_HSV']))
 
     return frame_threshold
 
--- tune_hsv.py
+++ tune_hsv.py
@@ -2,19 +2,15 @@ from __future__ import print_function
 import cv2 as cv
 import argparse
 from process import process
+import toml
 
 import numpy as np
 
+
+configs = toml.load("config.toml")["preprocessing"]
+
 max_value = 255
 max_value_H = 360 // 2
-low_H = 0
-low_S = 0
-low_V = 0
-high_H = max_value_H
-high_S = max_value
-high_V = max_value
-erosion = 0
-dilation = 0
 window_capture_name = 'Video Capture'
 window_detection_name = 'Object Detection'
 low_H_name = 'Low H'
@@ -26,63 +22,51 @@ high_V_name = 'High V'
 
 
 def on_low_H_thresh_trackbar(val):
-    global low_H
-    global high_H
-    low_H = val
-    low_H = min(high_H - 1, low_H)
-    cv.setTrackbarPos(low_H_name, window_detection_name, low_H)
+    global configs
+    configs['lower_HSV'][0] = min(configs['upper_HSV'][0] - 1, val)
+    cv.setTrackbarPos(low_H_name, window_detection_name, configs['lower_HSV'][0])
 
 
 def on_high_H_thresh_trackbar(val):
-    global low_H
-    global high_H
-    high_H = val
-    high_H = max(high_H, low_H + 1)
-    cv.setTrackbarPos(high_H_name, window_detection_name, high_H)
+    global configs
+    configs['upper_HSV'][0] = max(configs['lower_HSV'][0] + 1, val)
+    cv.setTrackbarPos(high_H_name, window_detection_name, configs['upper_HSV'][0])
 
 
 def on_low_S_thresh_trackbar(val):
-    global low_S
-    global high_S
-    low_S = val
-    low_S = min(high_S - 1, low_S)
-    cv.setTrackbarPos(low_S_name, window_detection_name, low_S)
+    global configs
+    configs['lower_HSV'][1] = min(configs['upper_HSV'][1] - 1, val)
+    cv.setTrackbarPos(low_S_name, window_detection_name, configs['lower_HSV'][1])
 
 
 def on_high_S_thresh_trackbar(val):
-    global low_S
-    global high_S
-    high_S = val
-    high_S = max(high_S, low_S + 1)
-    cv.setTrackbarPos(high_S_name, window_detection_name, high_S)
+    global configs
+    configs['upper_HSV'][1] = max(configs['lower_HSV'][1] + 1, val)
+    cv.setTrackbarPos(high_S_name, window_detection_name, configs['upper_HSV'][1])
 
 
 def on_low_V_thresh_trackbar(val):
-    global low_V
-    global high_V
-    low_V = val
-    low_V = min(high_V - 1, low_V)
-    cv.setTrackbarPos(low_V_name, window_detection_name, low_V)
+    global configs
+    configs['lower_HSV'][2] = min(configs['upper_HSV'][2] - 1, val)
+    cv.setTrackbarPos(low_V_name, window_detection_name, configs['lower_HSV'][2])
 
 
 def on_high_V_thresh_trackbar(val):
-    global low_V
-    global high_V
-    high_V = val
-    high_V = max(high_V, low_V + 1)
-    cv.setTrackbarPos(high_V_name, window_detection_name, high_V)
+    global configs
+    configs['upper_HSV'][2] = max(configs['lower_HSV'][2] + 1, val)
+    cv.setTrackbarPos(high_V_name, window_detection_name, configs['upper_HSV'][2])
 
 
 def on_erosion(val):
-    global erosion
-    erosion = val
-    cv.setTrackbarPos("Erosion", window_detection_name, erosion)
+    global configs
+    configs['erode'] = val
+    cv.setTrackbarPos("Erosion", window_detection_name, val)
 
 
 def on_dilation(val):
-    global dilation
-    dilation = val
-    cv.setTrackbarPos("Dilation", window_detection_name, dilation)
+    global configs
+    configs['erode'] = val
+    cv.setTrackbarPos("Dilation", window_detection_name, val)
 
 
 parser = argparse.ArgumentParser(description='Code for Thresholding Operations using inRange tutorial.')
@@ -91,29 +75,21 @@ args = parser.parse_args()
 cap = cv.VideoCapture(args.camera)
 cv.namedWindow(window_capture_name)
 cv.namedWindow(window_detection_name)
-cv.createTrackbar(low_H_name, window_detection_name, low_H, max_value_H, on_low_H_thresh_trackbar)
-cv.createTrackbar(high_H_name, window_detection_name, high_H, max_value_H, on_high_H_thresh_trackbar)
-cv.createTrackbar(low_S_name, window_detection_name, low_S, max_value, on_low_S_thresh_trackbar)
-cv.createTrackbar(high_S_name, window_detection_name, high_S, max_value, on_high_S_thresh_trackbar)
-cv.createTrackbar(low_V_name, window_detection_name, low_V, max_value, on_low_V_thresh_trackbar)
-cv.createTrackbar(high_V_name, window_detection_name, high_V, max_value, on_high_V_thresh_trackbar)
-cv.createTrackbar("Erosion", window_detection_name, erosion, 2, on_erosion)
-cv.createTrackbar("Dilation", window_detection_name, dilation, 2, on_dilation)
+cv.createTrackbar(low_H_name, window_detection_name, configs['lower_HSV'][0], max_value_H, on_low_H_thresh_trackbar)
+cv.createTrackbar(high_H_name, window_detection_name, configs['upper_HSV'][0], max_value_H, on_high_H_thresh_trackbar)
+cv.createTrackbar(low_S_name, window_detection_name, configs['lower_HSV'][1], max_value, on_low_S_thresh_trackbar)
+cv.createTrackbar(high_S_name, window_detection_name, configs['upper_HSV'][1], max_value, on_high_S_thresh_trackbar)
+cv.createTrackbar(low_V_name, window_detection_name, configs['lower_HSV'][2], max_value, on_low_V_thresh_trackbar)
+cv.createTrackbar(high_V_name, window_detection_name, configs['upper_HSV'][2], max_value, on_high_V_thresh_trackbar)
+cv.createTrackbar("Erosion", window_detection_name, configs['erode'], 2, on_erosion)
+cv.createTrackbar("Dilation", window_detection_name, configs['dilate'], 2, on_dilation)
 while True:
 
     ret, frame = cap.read()
     if frame is None:
         break
 
-    frame_threshold = process(frame,
-                              low_H=low_H,
-                              high_H=high_H,
-                              low_S=low_S,
-                              high_S=high_S,
-                              low_V=low_V,
-                              high_V=high_V,
-                              erode=erosion,
-                              dilate=dilation)
+    frame_threshold = process(frame, configs)
 
     cv.imshow(window_capture_name, frame)
     cv.imshow(window_detection_name, frame_threshold)
